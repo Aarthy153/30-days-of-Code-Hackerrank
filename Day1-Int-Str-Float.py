1397805e73f13d85af98fe6b21c33c2e324d76dd
Aarthy Ramesh <69035013+Aarthy153@users.noreply.github.com>
2022-08-16 08:16:12 -0700

    Day 1
    Print the sum of both integers on the first line, the sum of both doubles (scaled to 1 decimal place) on the second line, and then the two concatenated strings on the third line.

---
+++ Day1-Int-Str-Float.py
@@ -0,0 +1,17 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+integer = int(input())
+print(integer + i)
+# Print the sum of both integer variables on a new line.
+doubleNum = float(input())
+sumd = doubleNum + d
+# Print the sum of the double variables on a new line.
+print(sumd)
+# Concatenate and print the String variables on a new line
+string = input()
+print(s + string)
+# The 's' variable above should be printed first.
