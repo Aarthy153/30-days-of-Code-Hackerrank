65d031195925c1998c44cf5033f39fa88fbadc19
Aarthy Ramesh <69035013+Aarthy153@users.noreply.github.com>
2022-08-16 08:15:11 -0700

    Day 0
    # TODO: Write a line of code here that prints the contents of input_string to stdout.

---
+++ Day1-Hello World.py
@@ -0,0 +1,9 @@
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input()
+
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
+# Print a string literal saying "Hello, World." to stdout.
+# Using escape sequence
+print('Hello, World.' + "\n" + input_string)
+
+
